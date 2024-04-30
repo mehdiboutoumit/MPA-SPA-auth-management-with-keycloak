5fb4f6bfdbdc11c6193f4acdc721f292663f1776
HassanBouighamdane <hassanbouighamdane2002@gmail.com>
2024-04-30 18:52:03 +0100

    Adding JSF-MPA with a simple auth service and SPA-backend with spring boot

---
+++ SPA2/spa-backend/src/test/java/com/example/spabackend/SpaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
